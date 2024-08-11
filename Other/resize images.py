08c19fd3ba70c8acfc04d2c19220187f01a466ea
Awesome7285 <70990706+Awesome7285@users.noreply.github.com>
2024-08-11 21:41:25 +1000

    First Released Version
    Added Images, code changes, visual changes

---
+++ Other/resize images.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import shutil, os
+
+folder = r"F:\New Computer\Unity\MISSION IMAGES/Raw/"
+out = r"F:\New Computer\Unity\MISSION IMAGES/Resized/"
+
+for img in os.listdir(folder):
+    im = Image.open(folder+img)
+    im = im.resize((im.width//2, im.height//2))
+    im.save(out+img)
